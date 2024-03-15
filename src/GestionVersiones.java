c489340809591d23a5faa7880c1bc8dd6a9df8d1
Guille <guillermo.montaner@educa.madrid.org>
2024-03-15 13:55:37 +0100

    Vuelto a crear inicio proyecto

--- src/GestionVersiones.java
+++ src/GestionVersiones.java
@@ -3,7 +3,8 @@ public class GestionVersiones {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola Muncdo");
+		int variable =0;
 	}
 
 }
